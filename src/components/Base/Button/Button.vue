cc64be6dd7f502f14b4a0bdf4d3d21c798176942
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2023-03-09 20:52:44 -0300

    :lipstick: style: base button

---
+++ src/components/Base/Button/Button.vue
@@ -0,0 +1,13 @@
+<template>
+  <button class="bg-custom-blue text-white py-1 px-6 font-bold rounded-full lg:hover:bg-sky-600 duration-300">
+    <section class="uppercase">
+      <slot name="login" />
+    </section>
+    <section>
+      <slot />
+    </section>
+  </button>
+</template>
+
+<script lang="ts">
+</script>
