a807b1ab13b008e604080f2866485ab3886fe20d
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2023-03-09 20:13:48 -0300

    :package: chore: add meta and links tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1.0',
+      title: 'Workout Tracker App',
+      meta: [
+        { name: 'description', content: 'loren' },
+      ],
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;600;700;900&display=swap' },
+      ]
+    }
+  }
 })
