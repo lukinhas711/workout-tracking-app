a01b48a3c08149d63438a6f27f498d818a175a4e
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2023-03-09 23:24:27 -0300

    :bug:

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/components/**/*.{js,vue,ts}",
-    "./src/layouts/**/*.vue",
-    "./src/pages/**/*.vue",
-    "./src/plugins/**/*.{js,ts}",
+    ".components/**/*.{js,vue,ts}",
+    ".layouts/**/*.vue",
+    ".pages/**/*.vue",
+    ".plugins/**/*.{js,ts}",
     "./nuxt.config.{js,ts}",
   ],
   theme: {
