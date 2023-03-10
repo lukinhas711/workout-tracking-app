3228344b2c9081e5627b22c9a3b4094c039c3c65
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2023-03-09 22:27:47 -0300

    :bug: fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['./src/assets/css/global.css'],
+  css: ['~/assets/css/global.css'],
   srcDir: 'src/',
   app: {
     head: {
