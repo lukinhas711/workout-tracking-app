866b6f3b7b1e4e451fcdec36e6e9ecd00f9ce1a0
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2023-03-09 21:26:57 -0300

    :sparkles: feat: base container

---
+++ src/components/Base/Container/Container.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="container mx-auto">
+    <section class="flex items-center justify-center">
+      <slot name="center" />
+    </section>
+    <section class="flex items-center justify-between">
+      <slot name="between" />
+    </section>
+  </section>
+</template>
