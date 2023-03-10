7658343194fb6f03c1f51c8e256cc793478177c5
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2023-03-09 21:27:15 -0300

    :sparkles: feat: default layout

---
+++ src/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>
+    <TheMenu />
+    <main>
+      <BaseContainer>
+        <template #center>
+          <NuxtPage class="mt-[60px]" />
+        </template>
+      </BaseContainer>
+    </main>
+  </section>
+</template>
