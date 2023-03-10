243a932f1060cfd1c5915448bafb1395d73126e6
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2023-03-09 21:27:53 -0300

    :lipstick: style: menu

---
+++ src/components/The/Menu/Menu.vue
@@ -0,0 +1,11 @@
+<template>
+  <section
+    class="fixed bg-black-primary inset-x-0 h-[60px] py-2 px-6 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-[95]">
+    <BaseContainer>
+      <template #between>
+        <img src="" alt="Logo do app" width="100" class="bg-white h-10">
+        <BaseButton><template #login>Login</template></BaseButton>
+      </template>
+    </BaseContainer>
+  </section>
+</template>
