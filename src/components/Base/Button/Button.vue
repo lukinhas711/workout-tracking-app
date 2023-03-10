df02fdbb9ac0c7aff1bbf0957d5c20143ef658d7
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2023-03-09 22:54:08 -0300

    :lipstick: fix: postcss syntax

--- src/components/Base/Button/Button.vue
+++ src/components/Base/Button/Button.vue
@@ -1,6 +1,6 @@
 <template>
-  <button class="bg-custom-blue text-white py-1 px-6 font-bold rounded-full lg:hover:bg-sky-600 duration-300">
-    <nuxt-link :to="to" class="uppercase">
+  <button :class="style.button">
+    <nuxt-link :to="to" :class="$style.isLogin">
       <slot name="login" />
     </nuxt-link>
     <nuxt-link :to="to">
@@ -10,10 +10,19 @@
 </template>
 
 <script setup lang="ts">
-
 const props = defineProps({
   to: {
     type: String,
-  }
-})
+  },
+});
 </script>
+
+<style module lang="postcss">
+.button {
+  @apply bg-custom-blue text-white py-1 px-6 font-bold rounded-full lg:hover:bg-sky-600 duration-300;
+}
+
+.isLogin {
+  @apply uppercase;
+}
+</style>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -4,9 +4,15 @@
     <main>
       <BaseContainer>
         <template #center>
-          <NuxtPage class="mt-[80px]" />
+          <NuxtPage :class="$style.marginTop" />
         </template>
       </BaseContainer>
     </main>
   </section>
 </template>
+
+<style module lang="postcss">
+.marginTop {
+  @apply mt-[80px];
+}
+</style>
